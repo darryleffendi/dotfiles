803d9e603447bd0dbd15cacd9eed078c67b12a56
Darryl Effendi <darrylgate@gmail.com>
2026-04-15 17:31:21 +0000

    fix: wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -49,7 +49,46 @@ config.colors = {
 		"#9ccfd8", -- Bright Cyan
 		"#e0def4", -- Bright White
 	},
-}                 ground = background_with_wallpaper
+}
+
+-- Dimmer for background
+local dimmer = {
+	brightness = 0.05,
+	saturation = 0.75,
+}
+
+-- Define the two background options (wallpaper and transparent)
+local background_with_wallpaper = {
+	{
+		source = {
+			File = wez.config_dir .. "/backgrounds/chaeyoung-1.jpeg",
+			-- File = wez.config_dir .. "/backgrounds/pain.jpg",
+			-- File = wez.config_dir .. "/backgrounds/azumanga.gif",
+		},
+		width = "Cover",
+		height = "Cover",
+		vertical_align = "Middle",
+		horizontal_align = "Center",
+		repeat_x = "NoRepeat",
+		repeat_y = "NoRepeat",
+		opacity = 1,
+		hsb = dimmer,
+	},
+}
+
+local transparent_background = {
+	{
+		source = {
+			Color = "#0a0a0a",
+		},
+		width = "100%",
+		height = "100%",
+		opacity = 0.8,
+	},
+}
+
+-- Initialize with the wallpaper by default
+config.background = background_with_wallpaper
 
 -- config.background = transparent_background
 -- config.macos_window_background_blur = 30
@@ -93,40 +132,3 @@ config.window_padding = {
 config.native_macos_fullscreen_mode = false
 
 return config
-
--- Dimmer for background
-local dimmer = {
-	brightness = 0.05,
-	saturation = 0.75,
-}
-
--- Define the two background options (wallpaper and transparent)
-local background_with_wallpaper = {
-	{
-		source = {
-			File = wez.config_dir .. "/Users/darryl.effendi/Pictures/bg-matsuya-dark.jpg",
-		},
-		width = "Cover",
-		height = "Cover",
-		vertical_align = "Middle",
-		horizontal_align = "Center",
-		repeat_x = "NoRepeat",
-		repeat_y = "NoRepeat",
-		opacity = 1,
-		hsb = dimmer,
-	},
-}
-
-local transparent_background = {
-	{
-		source = {
-			Color = "#0a0a0a",
-		},
-		width = "100%",
-		height = "100%",
-		opacity = 0.8,
-	},
-}
-
--- Initialize with the wallpaper by default
-config.back
