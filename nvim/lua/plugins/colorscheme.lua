3d19a8472bd1c638c538e0735ac221e237186589
Darryl Effendi <darryl.effendi@mab.co.id>
2026-06-04 00:02:13 +0700

    nvim colorscheme and alpha dashboard

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "AlexvZyl/nordic.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/everforest" },
+  {
+    -- use kanagawa colorscheme for
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa",
+    },
+  },
+}
